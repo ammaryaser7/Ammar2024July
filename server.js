fd3579234493f3525e0959abfa441337b2d3657d
ammaryaser7 <42716279+ammaryaser7@users.noreply.github.com>
2024-07-10 13:56:34 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-//  I'm a comment , you should to pull me
+//  I'm a comment , okookkoko you should to pull me
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
