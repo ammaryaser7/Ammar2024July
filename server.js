a8a3396b2b9ad1ce009d4a105fa25c5e2131e5e3
ammar <ammar@DELLG3-AM17>
2024-07-10 12:41:57 +0400

    commit main

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
-// Yes , The main is here before rebase
+// Yes , The main is here before rebase,Ammar
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
