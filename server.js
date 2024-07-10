4d332fd9d80f2b0c8be7d6e4a220b53772c310d8
ammar <ammar@DELLG3-AM17>
2024-07-10 12:26:40 +0400

    Saving changes before rebase

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
-// Yes , The branch2 is here
+// Yes , The main is here before rebase
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
